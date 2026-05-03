13267f3cab19f4acf9a2cb5a52762bad1f889eba
Diya536 <diyatiwari536@gmail.com>
2026-05-03 22:55:36 +0530

    fixed user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,17 +2,31 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
 
-    name: String,
+    name: {
+
+        type: String,
+
+        required: true
+
+    },
 
     email: {
 
         type: String,
 
+        required: true,
+
         unique: true
 
     },
 
-    password: String,
+    password: {
+
+        type: String,
+
+        required: true
+
+    },
 
     role: {
 
@@ -25,8 +39,6 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model(
-
     "User",
     userSchema
-
 );
