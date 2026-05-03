9b5c3744258cfb3dd29787c9f7a46871d6e1d854
Diya536 <diyatiwari536@gmail.com>
2026-05-03 21:52:04 +0530

    fixed backend crash

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
 
     console.log("MongoDB Connected");
-
-    const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
 
